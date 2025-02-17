8642e9a61c1ad3033fc75918b9de5d638ef45987
lijin0303 <ruitong@broadinstitute.org>
2025-02-17 14:47:34 +0000

    update DEGpattern testdata

--- rnaseq/DEGpattern/Data_prep.R
+++ rnaseq/DEGpattern/Data_prep.R
@@ -1,7 +1,7 @@
 ## List all directories containing data  
 setwd("~/hbc_test")
 options(stringsAsFactors = F)
-pacman::p_load(DESeq2,tximport)
+pacman::p_load(DESeq2,tximport,glue)
 samples <- list.files(path = "./data", full.names = T, pattern="salmon$")
 files <- file.path(samples, "quant.sf")
 names(files) <- gsub(".*data/(.*)\\.salmon.*","\\1",files)
@@ -23,8 +23,18 @@ dds <- DESeqDataSetFromTximport(txi, colData = meta, design = ~ sampletype)
 #https://github.com/hbctraining/Intro-to-DGE/blob/master/lessons/02_DGE_count_normalization.md
 
 dds_lrt <- DESeq(dds, test="LRT", reduced = ~ 1)
+
+## Output list of DEGs with chosen padj.cutoff and topN, whichever gives the smaller length of genes 
+padj.cutoff <- 0.05
+topN <- 1000
+
+res_LRT <- results(dds_lrt) %>% na.omit()
+gene_padj <- setNames(res_LRT$padj,rownames(res_LRT))
+gene_sig <- gene_padj[gene_padj<padj.cutoff]
+degs <- sort(gene_sig)[1:min(length(gene_sig),topN)]
+
 saveRDS(dds_lrt,"DEGpattern_deseq_results.rds")
 saveRDS(dds,"DEGpattern_deseq_obj.rds")
 saveRDS(meta,"DEGpattern_deseq_meta.rds")
-
+saveRDS(degs,glue("DEGpattern_deseq_DEGs_padj{padj.cutoff}_topN{topN}.rds"))
 
